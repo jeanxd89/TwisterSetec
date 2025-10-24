c4a710e2d27a16109f3d1656753e869be9e2408e
simoees <simoes.oliveira.lais@escola.pr.gov.br>
2025-10-24 08:56:10 -0300

    Add TwisterGame component to Home page

--- página.tsx
+++ página.tsx
@@ -1,1 +1,10 @@
+import TwisterGame from "@/components/twister-game"
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-background">
+      <TwisterGame />
+    </main>
+  )
+}
 
